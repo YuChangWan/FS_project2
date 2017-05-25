f0f7bf1b842dc9d2c7294095eafcf1b3cfca8674
YuChangWan <dbckddhks13@naver.com>
2017-05-25 21:33:51 +0900

    complete hashfunction
    made class HashMap
    now making HashMap mapping method

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
+/*	File Struct Project 2 - dinamic hash and B+tree
+	this project started 2017.05.19
+	last update 2017.05.25 
+	in progress
+*/
+
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<cmath>
 using namespace std;
 #define BLOCK_SIZE 4096
 #define MAX_SIZE 50
-#define BIN_ID_SIZE 27
-char inputString[MAX_SIZE];
+//ID number is composited of numbers of 9, and binary format of 999999999 is 30bit, and + NULL
+#define BIN_ID_SIZE 31
 
+void tempHashFunc(int recID, char* bin_ID);
 struct Student {
 	char name[20];
 	unsigned studentID;
@@ -25,30 +33,46 @@ public:
 	//constructor and assign method for input a record 
 	Class_Student(char* str) { assign_Student(&stud, str);}
 	void Assign(char* str) { assign_Student(&stud, str);}
-};
 
-void tempHashFunc(int recID, int* bin_ID) {
-	int remainder;
-	for (int i = 0; i < BIN_ID_SIZE; i++) {
-		if (recID <= 1) {
-			bin_ID[i] =  recID;
-			break;
-		}
-		remainder = recID % 2;
-		bin_ID[i] = remainder;
-		recID >> 1;
-	}
+	int getID() { return stud.studentID; }
+};
+class HashMap {
+private:
+	struct Pair {	//because two demention array evoke confusion
+		int address;
+		int prefix;	//hashprefix of a block
+	};
+	Pair *map;
+	int mapsize;	//2^maxprefix (it is not nessesary maybe..)
+	int maxprefix;	//biggest hashprefix among every block 
 	
-	return;
+public:
+	HashMap():mapsize(0),maxprefix(0){}
+	~HashMap() { if(!mapsize)delete map; }
+	void insert(int);
+};
+void HashMap::insert(int recID) {
+	int mid_address = 0;
+	char bin_ID[BIN_ID_SIZE];
+	tempHashFunc(recID, bin_ID);
+	for (int i = 0; i < maxprefix; i++) {
+		mid_address += (bin_ID[i]-'0')*pow(2,i);
+	}
+	/*	now seek the real adrress by using file pointer.
+		TODO
+		overflow control
+		some work , if maxprefix > pair.prefix 
+	*/
 }
+
 int main() {
+	char inputString[MAX_SIZE];
+	char bin_ID[BIN_ID_SIZE];
 	Class_Student student;
-	string s;
-	int bin_ID[BIN_ID_SIZE];
-	int N;
+	int N;	//number of students 
 
 	ifstream DB("sampleData.csv");
-
+	
 
 	DB.getline(inputString, sizeof(char)*MAX_SIZE);
 	N = atoi(inputString);
@@ -59,8 +83,8 @@ int main() {
 	for (int i = 0; i<N && !DB.eof(); i++) {
 		DB.getline(inputString, sizeof(char)*MAX_SIZE);
 		student.Assign(inputString);
-		cout << i <<" "<<student;
-
+		tempHashFunc(student.getID(), bin_ID);
+		cout << i <<" "<<student<<" "<<bin_ID<<endl;
 	}
 
 	DB.close();
@@ -72,7 +96,7 @@ int main() {
 
 
 ostream& operator<<(ostream& stream, const Class_Student& s1) {
-	stream << s1.stud.studentID << " " << s1.stud.name << " " << s1.stud.score << " " << s1.stud.advisorID << endl;
+	stream << s1.stud.studentID << " " << s1.stud.name << " " << s1.stud.score << " " << s1.stud.advisorID;
 	return stream;
 }
 
@@ -86,5 +110,24 @@ void assign_Student(Student *stud, char *str) {		//input record to struct Studen
 	stud->score = atof(token);
 	token = strtok_s(NULL, ",", &context);
 	stud->advisorID = atoi(token);
+	return;
+}
+
+void tempHashFunc(int recID, char* bin_ID) {
+	int remainder;									//convert (int)studentID to binary format
+	bin_ID[BIN_ID_SIZE - 1] = NULL;					//least significant bit is bin_ID[0]
+	for (int i = 0; i < BIN_ID_SIZE - 1; i++) {		//if 24 -> 11000(2) -> "00011 + 000..."
+		bin_ID[i] = '0';
+	}
+	for (int i = 0; i < BIN_ID_SIZE - 1; i++) {
+		if (recID <= 1) {
+			bin_ID[i] = recID + '0';
+			break;
+		}
+		remainder = recID % 2;
+		bin_ID[i] = remainder + '0';
+		recID = recID >> 1;
+	}
+
 	return;
 }
